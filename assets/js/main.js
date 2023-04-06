1141e2f408095d808a9964303354bf545b738cb8
Irwing <irwingcorrea95@gmail.com>
2023-04-06 13:56:29 -0300

    commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('listaAtividades')
+const nomeAtividades = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha(){
+    const inputNomeAtividades = document.getElementById('nomeAtividades')
+
+    if(nomeAtividades.includes(inputNomeAtividades.value)) {
+        alert(`O contato já foi inserido`)
+    } else {
+        nomeAtividades.push(inputNomeAtividades.value);
+    
+        let linha = '<ul>';
+        linha += `<li>${inputNomeAtividades.value}</li>`;
+        linha += `</ul>`;
+    
+        linhas += linha
+    }
+
+
+    inputNomeAtividades.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('ul')
+    corpoTabela.innerHTML = linhas;
+}
+
+$(document).ready(function() {
+    $('button').click(function(){
+        $('form').slideDown();
+    })
+})
